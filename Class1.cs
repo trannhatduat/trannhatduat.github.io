a6daf51d555507cdc4809eeaaf292838e7602b48
trannhatduat <67281144+trannhatduat@users.noreply.github.com>
2020-06-29 13:53:03 +0700

    bai1

---
+++ Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bai1
+{
+    class TComplex
+    {
+        int thuc, ao;
+
+        public TComplex()
+        {
+            thuc = 1;
+            ao = 2;
+        }
+
+        public TComplex(int t, int a)
+        {
+            thuc = t;
+            ao = a;
+        }
+
+        public void input()
+        {
+            Console.WriteLine("Nhap so phuc: ");
+            Console.Write("thuc = ");
+            thuc = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ao = ");
+            ao = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void output()
+        {
+            Console.WriteLine("{0} + {1}i", thuc, ao);
+        }
+
+        public static TComplex operator +(TComplex a, TComplex b)
+        {
+            TComplex c = new TComplex();
+            c.thuc = a.thuc + b.thuc;
+            c.ao = a.ao + b.ao;
+            return c;
+        }
+        public static TComplex operator -(TComplex a, TComplex b)
+        {
+            TComplex c = new TComplex();
+            c.thuc = a.thuc - b.thuc;
+            c.ao = a.ao - b.ao;
+            return c;
+        }
+        public static TComplex operator *(TComplex a, TComplex b)
+        {
+            TComplex c = new TComplex();
+            c.thuc = a.thuc * b.thuc - a.ao * b.ao;
+            c.ao = a.ao * b.thuc + a.thuc * b.ao;
+            return c;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            TComplex sp1, sp2, sp3, sp4, sp5;
+
+            sp1 = new TComplex();
+
+            Console.Write("sp1 = ");
+            sp1.input();
+
+            sp2 = new TComplex(2, 3);
+            Console.Write("sp2 = ");
+            sp2.input();
+            Console.Write("sp1 = ");
+            sp1.output();
+            Console.Write("sp2 = ");
+            sp2.output();
+
+            sp3 = sp1 - sp2;
+            Console.Write("sp3 = sp1 - sp2 = ");
+            sp3.output();
+
+            sp4 = sp1 + sp2;
+
+            Console.Write("sp4 = sp1 + sp2 = ");
+            sp4.output();
+
+            sp5 = sp4 * sp3;
+            Console.Write("sp5 = sp4 * sp3 = ");
+            sp5.output();
+
+        }
+    }
+}
